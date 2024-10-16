b37b06bb0a4c1c78c18c2fb7f96653fc280e905e
belegisanin <belegisanind@gmail.com>
2024-10-16 19:13:35 +0200

    feat: Implemented Accounts component
    - Added the Accounts component
    - Expanded the Administration service
    - Added the Acount model interface
    - Routed the component to /accounts

---
+++ Explorer/src/app/feature-modules/administration/accounts/accounts.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Account } from '../model/account.model';
+import { MatTableModule } from '@angular/material/table';
+import { AdministrationService } from '../administration.service';
+import { MatButton } from '@angular/material/button';
+
+@Component({
+  selector: 'xp-accounts',
+  templateUrl: './accounts.component.html',
+  styleUrls: ['./accounts.component.css'],
+})
+export class AccountsComponent implements OnInit {
+  
+  constructor(private administrationService: AdministrationService) {}
+
+  displayedColumns: string[] = [ 'username', 'email', 'role', 'status', 'action'];
+  accounts: Account[] = []
+
+  ngOnInit(): void {
+    this.administrationService.getAccounts().subscribe(
+      (data) => this.accounts = data.results,
+      (error) => console.error(error)
+    )
+  }
+}
