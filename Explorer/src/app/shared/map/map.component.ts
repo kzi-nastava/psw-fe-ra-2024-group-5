0fcae544eae7d4dd34d0fbe1bfb9f34c0559a4e7
HakTak <katek.m536@gmail.com>
2024-10-21 10:15:53 +0200

    feat: Enabling using tour maps for keypoints and objects - able to select multiple locations for keypoints and shows it's route from 1st to last location - able to select just one object and then show route from tourists location to object - able to return and change markers and values

--- Explorer/src/app/shared/map/map.component.ts
+++ Explorer/src/app/shared/map/map.component.ts
@@ -8,7 +8,10 @@ import * as L from 'leaflet';
   styleUrls: ['./map.component.css'],
 })
 export class MapComponent implements AfterViewInit {
-  private map: any;
+  private map: any; 
+  private markers: L.Marker[] = [];
+  private routeControl: any;
+  private isLastMarkerSet: boolean;
 
   constructor(private mapService: MapService) {}
 
@@ -17,8 +20,11 @@ export class MapComponent implements AfterViewInit {
       center: [45.2396, 19.8227],
       zoom: 13,
     });
-    this.registerOnClick()
-    this.setRoute()
+    this.isLastMarkerSet = false;
+
+    this.registerOnClick(false); //false ako je u pitanju 'keyPpoint', true ako je 'object'
+    // this.search()
+    // this.setRoute(L.latLng(57.74, 11.94), L.latLng(57.6792, 11.949))
     
     const tiles = L.tileLayer(
       'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -42,22 +48,27 @@ export class MapComponent implements AfterViewInit {
 
     this.initMap();
     //ovaj search cemo pozivati kada dodamo neki textbox za search ili tako nesto
-    this.search();
+    // this.search();
   }
 
-  search(): void {
-    this.mapService.search('Strazilovska 19, Novi Sad').subscribe({
+  search(isObject: boolean, searchInput: string): void {
+    this.mapService.search('Strazilovska 19, Novi Sad').subscribe({  //searchInput umesto ovoga
       next: (result) => {
-        L.marker([result[0].lat, result[0].lon])
-          .addTo(this.map)
-          .bindPopup('Pozdrav iz Strazilovske 19.')
-          .openPopup();
+        if(isObject){
+          this.addObjectMarker([result[0].lat, result[0].lon], 'Pozdrav iz Strazilovske 19.');
+        }else{
+          this.addKeyPointMarker([result[0].lat, result[0].lon], 'Pozdrav iz Strazilovske 19.');
+        }
+        // L.marker([result[0].lat, result[0].lon])
+        //   .addTo(this.map)
+        //   .bindPopup('Pozdrav iz Strazilovske 19.')
+        //   .openPopup();
       },
       error: () => {},
     });
   }
 
-  registerOnClick(): void {
+  registerOnClick(isObject: boolean): void {
     this.map.on('click', (e: any) => {
       const coord = e.latlng;
       const lat = coord.lat;
@@ -66,23 +77,92 @@ export class MapComponent implements AfterViewInit {
         //console.log(res.display_name);
         // RES se koristi da prikaze ulicu grad.. od mesta koje je selektovano
       });
-      const mp = new L.Marker([lat, lng]).addTo(this.map);
-      alert(mp.getLatLng());
+      if(isObject){
+        this.addObjectMarker([lat, lng], 'New Marker')
+      }else{
+        this.addKeyPointMarker([lat, lng], 'New Marker');
+      }
+      // alert(mp.getLatLng());
     });
   }
 
+  addObjectMarker(latlng: [number, number], popupText: string): void {
+    if(!this.isLastMarkerSet && this.markers.length !== 0){
+      this.removeLastMarker();
+    }else{
+      if(this.markers.length !== 0){
+        this.setRouteToObject([45.2396, 19.8227])
+      }else{
+        const marker = new L.Marker(latlng).addTo(this.map).bindPopup(popupText);
+        this.markers.push(marker);
+      }
+    }
+  }
+
+  addKeyPointMarker(latlng: [number, number], popupText: string): void {
+
+    if(!this.isLastMarkerSet && this.markers.length !== 0){
+      this.removeLastMarker();
+    }
+    const marker = new L.Marker(latlng).addTo(this.map).bindPopup(popupText);
+    this.markers.push(marker);
+    // Check if we have two markers
+    if (this.markers.length >= 2) {
+      this.setRoute();
+    }
+  }
+  
+  setRouteToObject(myLocation: [number, number]){ //ovo ce biti ruta od turiste do objekta
+    
+    const marker = new L.Marker(myLocation).addTo(this.map).bindPopup("Moja lokacija");
+    this.markers.unshift(marker);
+    this.setRoute()
+  }
+
   setRoute(): void {
-    const routeControl = L.Routing.control({
-      waypoints: [L.latLng(57.74, 11.94), L.latLng(57.6792, 11.949)],
+    if (this.routeControl) {
+      this.routeControl.remove();
+    }
+    
+    const wPoints = this.markers.map(marker => marker.getLatLng());
+
+    this.routeControl = L.Routing.control({
+      waypoints: wPoints, //L.latLng(57.74, 11.94), L.latLng(57.6792, 11.949)
       router: L.routing.mapbox('pk.eyJ1IjoiaGFrdGFrIiwiYSI6ImNtMmk0MnZ5NDAwOWcybHNnN2N4dHRubnAifQ.kOERM4mimLJzQay3IqWDpw', {profile: 'mapbox/walking'}) //ova 'pk' kobasica je token sa mapbox sajta za koji sam nalog napravio preko uns maila, ako ne radi vama probajte napraviti nalog ima link u 2.4 lekciji
     }).addTo(this.map);
 
-    routeControl.on('routesfound', function(e) {
+    this.routeControl.on('routesfound', function(e: { routes: any; }) {
       var routes = e.routes;
       var summary = routes[0].summary;
       alert('Total distance is ' + summary.totalDistance / 1000 + ' km and total time is ' + Math.round(summary.totalTime % 3600 / 60) + ' minutes');
     });
   }
+    confirmMarker(): void{
+      this.isLastMarkerSet = true;
+    }
 
+    getRouteControl(): any {
+      return this.routeControl; // Return the current routeControl instance
+    }
+  
+    removeMarkers(): void{ 
+      this.markers.forEach(marker => {
+        this.map.removeLayer(marker);
+      });
+      this.markers = []; // Clear markers from array
+      this.isLastMarkerSet = false;
+    }
+
+    removeLastMarker(): void{ //ovo se poziva kada hocete da promenite lokaciju markera, ili izbrisete poslednji
+      const m = this.markers.pop()
+      this.map.removeLayer(m)
+    }
+
+  removeRoute(): void {
+    if (this.routeControl) {
+      this.routeControl.remove();
+      this.routeControl = null; // Clear the reference
+    }
+  }
 
 }
