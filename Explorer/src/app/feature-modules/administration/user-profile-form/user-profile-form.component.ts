3531dcf1f0de6b76ad12a164f05e94080f6d2dfb
ra50ivana <ivanaglamocic666@gmail.com>
2024-10-22 23:35:55 +0200

    Minor updates

--- Explorer/src/app/feature-modules/administration/user-profile-form/user-profile-form.component.ts
+++ Explorer/src/app/feature-modules/administration/user-profile-form/user-profile-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit, Input  } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { TokenStorage } from '../../../infrastructure/auth/jwt/token.service';
 import { UserProfile } from '../model/userProfile.model';
 import { UserProfileService } from '../user-profile.service';
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./user-profile-form.component.css']
 })
 export class UserProfileFormComponent implements OnInit {
+  @Input() userName: string;
   profileForm: FormGroup;
   userProfile: UserProfile;
   id: number;
@@ -20,19 +21,17 @@ export class UserProfileFormComponent implements OnInit {
 
     ngOnInit(): void {
       this.profileForm = this.fb.group({
-          name: [''],
-          surname: [''],
-          biography: [''],
-          motto: ['']
+        name: ['', Validators.required],
+        surname: ['', Validators.required],
+        biography: [''],
+        motto: ['']
       });
   
       const userId = this.tokenStorage.getUserId();
   
       if (userId) {
-        console.log("bla");
           this.service.getUserProfile(+userId).subscribe({
               next: (result: UserProfile) => {
-                console.log("bla");
                   this.userProfile = result;
                   this.id = this.userProfile.id;
                   if (this.userProfile) {
@@ -67,10 +66,17 @@ export class UserProfileFormComponent implements OnInit {
         name: this.profileForm.value.name,
         surname: this.profileForm.value.surname,
         biography: this.profileForm.value.biography,
-        motto: this.profileForm.value.motto
+        motto: this.profileForm.value.motto,
+        profilePictureUrl: this.userProfile.profilePictureUrl
       };
 
-      if(userId !== null) {
+      if(this.profileForm.invalid)
+      {
+        this.profileForm.markAllAsTouched(); 
+        return;
+      }
+
+      if(userId !== null && this.profileForm.valid) {
         this.service.updateUserProfile(userId, updatedProfile).subscribe({
           next: (response) => {
             this.router.navigate(['profile']);
