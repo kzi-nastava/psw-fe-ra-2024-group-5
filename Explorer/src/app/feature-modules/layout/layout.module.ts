b7ce86b0621a551a15c9f7cfd2ff5c32ec93e6c0
belegisanin <belegisanind@gmail.com>
2024-10-23 16:28:14 +0200

    feat: Global styles and assets
    - Global styles defined
    - Added static image assets

--- Explorer/src/app/feature-modules/layout/layout.module.ts
+++ Explorer/src/app/feature-modules/layout/layout.module.ts
@@ -4,16 +4,22 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MaterialModule } from 'src/app/infrastructure/material/material.module';
 import { RouterModule } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import { LandingComponent } from './landing/landing.component';
+import { CardLinkComponent } from './card-link/card-link.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    LandingComponent,
+    CardLinkComponent
   ],
   imports: [
     CommonModule,
     MaterialModule,
-    RouterModule
+    RouterModule,
+    MatIconModule
   ],
   exports: [
     NavbarComponent,
