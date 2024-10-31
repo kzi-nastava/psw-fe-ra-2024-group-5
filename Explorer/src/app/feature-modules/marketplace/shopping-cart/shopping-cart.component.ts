6c1705130ad93ebf8f8ab3beb1773cec9b6991e0
stamenkovicx <natasastamenkovic99@gmail.com>
2024-10-31 23:47:02 +0100

    feat: implemented removing item from shopping card

--- Explorer/src/app/feature-modules/marketplace/shopping-cart/shopping-cart.component.ts
+++ Explorer/src/app/feature-modules/marketplace/shopping-cart/shopping-cart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ShoppingCartService } from './shopping-cart.service';
 import { ShoppingCart } from '../model/shopping-cart.model';
 import { Currency } from '../model/money.model';
+import { OrderItem } from '../model/order-item.model';
 
 @Component({
   selector: 'xp-shopping-cart',
@@ -27,4 +28,25 @@ getCartItems(): void {
     error => console.error('Error fetching cart items', error)
   );
 }
+removeItem(item: OrderItem): void {
+  if (this.shoppingCart) {
+    const index = this.shoppingCart.items.indexOf(item);
+    if (index > -1) {
+      this.shoppingCart.items.splice(index, 1); 
+    }
+  }
+
+  this.shoppingCartService.removeItemFromCart(item, this.touristId).subscribe(
+    response => {
+      console.log('Item removed successfully', response);
+    },
+    error => {
+      console.error('Error removing item', error);
+      if (this.shoppingCart) {
+        this.shoppingCart.items.push(item); 
+      }
+    }
+  );
+}
+
 }
