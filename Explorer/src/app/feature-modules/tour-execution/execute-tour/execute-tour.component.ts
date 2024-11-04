b9d79163f2a317e2e838a1fb87fad0515e579779
nkusla <nikola.kuslakovic1616@gmail.com>
2024-11-05 00:39:29 +0100

    refac: Refactored map components so it accepts keyPoints as parameter
    - KeyPoints and UserLocation are now display as separate icons
    - Added neccessary service calls in order to get key point location

--- Explorer/src/app/feature-modules/tour-execution/execute-tour/execute-tour.component.ts
+++ Explorer/src/app/feature-modules/tour-execution/execute-tour/execute-tour.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { TokenStorage } from 'src/app/infrastructure/auth/jwt/token.service';
 import { TourExecutionService } from '../tour-execution.service';
 import { TourExecution } from '../model/tour-execution.model';
+import { Tour } from 'src/app/feature-modules/tour-authoring/model/tour.model';
+import { TourAuthoringService } from '../../tour-authoring/tour-authoring.service';
 
 @Component({
   selector: 'xp-execute-tour',
@@ -11,8 +13,12 @@ import { TourExecution } from '../model/tour-execution.model';
 export class ExecuteTourComponent {
   noActiveTours: boolean = true;
   tourExecution: TourExecution;
+  tour: Tour;
 
-  constructor(private service: TourExecutionService, private tokenStorage: TokenStorage) { }
+  constructor(
+    private service: TourExecutionService,
+    private tourService: TourAuthoringService,
+    private tokenStorage: TokenStorage) { }
 
   ngOnInit(): void {
     const userId = this.tokenStorage.getUserId();
@@ -23,6 +29,18 @@ export class ExecuteTourComponent {
       next: tourExecution => {
         this.tourExecution = tourExecution;
         this.noActiveTours = false;
+        const tourId = this.tourExecution.tourId;
+        if (!tourId)
+          return;
+
+        this.tourService.getTourForTouristById(tourId, userId).subscribe({
+          next: tour => {
+            this.tour = tour.tour;
+          },
+          error: error => {
+            console.log('Error fetching tour:', error.status);
+          }
+        });
       },
       error: error => {
         this.noActiveTours = true;
