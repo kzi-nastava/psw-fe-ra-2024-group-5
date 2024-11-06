8ea8823d272533a8ca1e078c12bf56255dc848b9
HakTak <katek.m536@gmail.com>
2024-11-07 00:11:53 +0100

    feat: Adding multiple durations for different transport

--- Explorer/src/app/feature-modules/tour-authoring/tour-creation/tour-creation.component.ts
+++ Explorer/src/app/feature-modules/tour-authoring/tour-creation/tour-creation.component.ts
@@ -8,6 +8,7 @@ import { User } from 'src/app/infrastructure/auth/model/user.model';
 import { KeyPointsComponent } from '../key-points/key-points.component';
 import { MapComponent } from 'src/app/shared/map/map.component';
 import { TourLevel, Currency, TourTransport } from '../model/tour.enums'; // Import the enums
+import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'xp-tour-creation',
@@ -15,16 +16,20 @@ import { TourLevel, Currency, TourTransport } from '../model/tour.enums'; // Imp
   styleUrls: ['./tour-creation.component.css']
 })
 export class TourCreationComponent {
+
   form: FormGroup;
+  formTransport : FormGroup;
   tourLevels: string[] = ['Beginner', 'Intermediate', 'Advanced'];
   tourTransports: string[] = ['On Foot', 'Bicycle', 'Car']; // Only the string representations
   tourLength: number | 0;
-  tourTransportDurations : TransportDuration[] = [];
+  tourDurationTransports: TransportDuration[]  = [];
   author: User | undefined;
   coordinates: number[] | null = null;
   @ViewChild(KeyPointsComponent) keyPointsListComponent!: KeyPointsComponent;
   @ViewChild(MapComponent) map: MapComponent;
+  @ViewChild(MatTable) table: MatTable<TransportDuration>;
 
+  displayedColumns: string[] = [ 'transportDuration', 'transport', 'deleteButton'];
 
   constructor(
     private formBuilder: FormBuilder,
@@ -33,6 +38,7 @@ export class TourCreationComponent {
     private router: Router
   ) {
     this.resetForm();
+    this.resetForTransport();
   }
 
   sendCordinates(latlng: number[]) {
@@ -46,24 +52,58 @@ export class TourCreationComponent {
     console.log(this.tourLength)
   }
 
-  setTransportDuration(data: { profile: string; time: number; }){
-
-    switch(data.profile){
-      case 'walking':
-        var td : TransportDuration = {duration: data.time, transport: TourTransport.OnFoot}
-        this.tourTransportDurations.push(td)
+  addTransportDuration(){
+    if (!this.formTransport.valid) {
+      this.formTransport.markAllAsTouched();
+      return;
+    }
+    const formTransportValue = this.formTransport.value;
+    let index: number | undefined;
+    let transportValue: number | undefined;
+    switch (formTransportValue.Transport) {
+      case 'On Foot':
+        transportValue = 0;
+        index = this.tourTransports.findIndex(t => t == formTransportValue.Transport);
+        this.tourTransports.splice(index, 1)
         break;
-      case 'cycling':
-        var td : TransportDuration = {duration: data.time, transport: TourTransport.Bicycle}
-        this.tourTransportDurations.push(td)
+      case 'Bicycle':
+        transportValue = 1;
+        index = this.tourTransports.findIndex(t => t == formTransportValue.Transport);
+        this.tourTransports.splice(index, 1)
         break;
-      case 'driving':
-        var td : TransportDuration = {duration: data.time, transport: TourTransport.Car}
-        this.tourTransportDurations.push(td)
+      case 'Car':
+        transportValue = 2;
+        index = this.tourTransports.findIndex(t => t == formTransportValue.Transport);
+        this.tourTransports.splice(index, 1)
         break;
       default:
-        return;
+        console.error('Invalid transportation selected');
+        return; // Early exit on invalid level
     }
+    this.tourDurationTransports.push({ duration: formTransportValue.Duration, transport: transportValue })
+    console.log(this.tourDurationTransports)
+    this.table.renderRows();
+    this.resetForTransport();
+  }
+  deleteTourTransport(transport: TourTransport){
+    switch (transport) {
+      case TourTransport.OnFoot:
+        this.tourTransports.push('On Foot')
+        break;
+      case TourTransport.Bicycle:
+        this.tourTransports.push('Bicycle')
+        break;
+      case TourTransport.Car:
+        this.tourTransports.push('Car')
+        break;
+      default:
+        console.error('Deleted Nothing!');
+        return; // Early exit on invalid level
+    }
+    const index = this.tourDurationTransports.findIndex(t => t.transport == transport)
+    this.tourDurationTransports.splice(index, 1)
+    this.table.renderRows();
+
   }
 
   cancelKeyPoint(latlng: number[]) {
@@ -78,14 +118,19 @@ export class TourCreationComponent {
     if (!this.form.valid) {
       this.form.markAllAsTouched();
       return;
+    }else if(this.tourDurationTransports.length == 0){
+      this.resetForTransport();
+      this.formTransport.markAllAsTouched();
+      return;
+    }else if(this.keyPointsListComponent.getKeyPoints().length == 0){
+      alert("Add at least one key point")
+      return;
     }
     this.authService.user$.subscribe(user => {
       this.author = user;
     });
 
     const formValue = this.form.value;
-
-    
 
     let levelValue: number | undefined;
     switch (formValue.Level) {
@@ -103,23 +148,6 @@ export class TourCreationComponent {
         return; // Early exit on invalid level
     }
 
-
-    let transportValue: number | undefined;
-    switch (formValue.Transport) {
-      case 'On Foot':
-        transportValue = 0;
-        break;
-      case 'Bicycle':
-        transportValue = 1;
-        break;
-      case 'Car':
-        transportValue = 2;
-        break;
-      default:
-        console.error('Invalid transportation selected');
-        return; // Early exit on invalid level
-    }
-
     let tour: TourCreation = {
       name: formValue.Name,
       description: formValue.Description,
@@ -128,7 +156,7 @@ export class TourCreationComponent {
       authorId: this.author?.id,
       length: this.tourLength,
       keyPoints: this.keyPointsListComponent.getKeyPoints(), // This should be populated based on key points
-      transportDurationDtos: this.tourTransportDurations, // Use selected transport
+      transportDurationDtos: this.tourDurationTransports, // Use selected transport
       
     };
 
@@ -151,6 +179,10 @@ export class TourCreationComponent {
           this.map.removeRoute();
         }
         this.resetForm();
+        this.resetForTransport();
+        this.tourDurationTransports.length = 0
+        this.table.renderRows()
+        console.log(this.tourDurationTransports)
       },
       error: (error) => {
         console.error('Error adding tour:', error);
@@ -173,10 +205,15 @@ export class TourCreationComponent {
       Name: ['', Validators.required],
       Description: ['', Validators.required],
       Tags: ['', Validators.required],
-      Level: ['', Validators.required],
-      TransportDuration: ['', [Validators.required, Validators.min(0)]],
-      Transport: ['', Validators.required]
+      Level: ['', Validators.required]
     });
+  }
+
+  resetForTransport(): void{
+    this.formTransport = this.formBuilder.group({
+      Duration: ['', [Validators.required, Validators.min(0)]],
+      Transport: ['', Validators.required]
+    })
   }
 
   back(): void {
