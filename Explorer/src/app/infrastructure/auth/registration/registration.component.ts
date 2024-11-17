7504829b3cd0d62a5d69d6d2b81eae44bf347a81
belegisanin <belegisanind@gmail.com>
2024-11-17 16:51:03 +0100

    refac: Register page refactored
    - Validation messages
    - Styling conventions
    - Error message
    - Global error styling

--- Explorer/src/app/infrastructure/auth/registration/registration.component.ts
+++ Explorer/src/app/infrastructure/auth/registration/registration.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Registration } from '../model/registration.model';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'xp-registration',
@@ -13,16 +14,27 @@ export class RegistrationComponent {
 
   constructor(
     private authService: AuthService,
-    private router: Router
-  ) {}
+    private router: Router,
+    private snackBarService: MatSnackBar
+  ) { }
 
   registrationForm = new FormGroup({
     name: new FormControl('', [Validators.required]),
     surname: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
     username: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required]),
   });
+
+  get name() { return this.registrationForm.get('name') }
+
+  get surname() { return this.registrationForm.get('surname') }
+
+  get email() { return this.registrationForm.get('email') }
+
+  get username() { return this.registrationForm.get('username') }
+
+  get password() { return this.registrationForm.get('password') }
 
   register(): void {
     const registration: Registration = {
@@ -38,6 +50,9 @@ export class RegistrationComponent {
         next: () => {
           this.router.navigate(['home']);
         },
+        error: () => {
+          this.snackBarService.open('An error occured. Try again later.', 'OK', { duration: 5000 })
+        }
       });
     }
   }
