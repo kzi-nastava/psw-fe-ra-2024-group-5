e6c17f957b1b78db2ef065d3d48c7c5639acbb05
Djukic <strahinjadjukic95@gmail.com>
2024-12-25 17:29:29 +0100

    feat: Wheel

--- Explorer/src/app/shared/shared.module.ts
+++ Explorer/src/app/shared/shared.module.ts
@@ -3,17 +3,19 @@ import { CommonModule } from '@angular/common';
 import { MapComponent } from './map/map.component';
 import { MaterialModule } from 'src/app/infrastructure/material/material.module';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { PrizeWheelComponent } from './prize-wheel/prize-wheel.component';
 
 
 @NgModule({
   declarations: [
-    MapComponent
+    MapComponent,
+    PrizeWheelComponent
   ],
   imports: [
     CommonModule,
     MaterialModule,
     MatCheckboxModule
   ],
-  exports: [MapComponent]
+  exports: [MapComponent,PrizeWheelComponent]
 })
 export class SharedModule { }
